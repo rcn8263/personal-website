63f04a8f52184766f5ad4c3fec69b0f7ef9d344b
rcn8263 <rcn8263@rit.edu>
2022-03-24 23:37:39 -0400

    mock data for each project displays on the web page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Project } from '../project';
+import { PROJECTS } from '../mock-projects';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+
+  projects = PROJECTS;
 
   constructor() { }
 
