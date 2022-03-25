8c49b076497618002ddf3a1f8162bb6934158061
rcn8263 <rcn8263@rit.edu>
2022-03-24 23:23:13 -0400

    created mock data for projects

---
+++ src/app/mock-projects.ts
@@ -0,0 +1,10 @@
+import { Project } from './project';
+
+export const PROJECTS: Project[] = [
+  {name: "project1", description: "this is a description for project 1", images: ["imageURL1"], link: "link1"},
+  {name: "project2", description: "this is a description for project 2", images: ["imageURL1", "imageURL2"], link: "link2"},
+  {name: "project3", description: "this is a description for project 3", images: ["imageURL1", "imageURL2", "imageURL3"], link: "link3"},
+  {name: "project4", description: "this is a description for project 4", images: ["imageURL1"], link: "link4"},
+  {name: "project5", description: "this is a description for project 5", images: ["imageURL1", "imageURL2"], link: "link5"},
+  {name: "project6", description: "this is a description for project 6", images: ["imageURL1", "imageURL2", "imageURL3"], link: "link6"},
+];
